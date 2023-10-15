bea9e8c41583d92bb29b2f43f363de4f81a35cbb
MityaBekreev <124333119+MityaBekreev@users.noreply.github.com>
2023-10-15 14:47:21 +0900

    Update task_2.py

--- Лабораторная работа 1/task_2.py
+++ Лабораторная работа 1/task_2.py
@@ -1,4 +1,8 @@
-volume = 1.44 * 2**20
+disk_size = 1.44 * 1024 * 1024
+bytes_char = 4
+pages = 100
+lines = 50
+chars = 25
 size_book = 100 * 50 * 25 * 4
-number_of_books = volume // size_book
-print("Количество книг, помещающихся на дискету:", round(number_of_books))
+number_of_books = int(disk_size // size_book)
+print("Количество книг, помещающихся на дискету:", number_of_books)
